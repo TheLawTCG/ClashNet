080ba70216423e204ffe62fae8072222d2ff6621
TheLawTCG <TheLawTCG@gmail.com>
2025-03-03 00:37:16 -0500

    Cache Fix V2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,53 @@
 const cacheName = "TheLawTCG-ClashNet-1.001";
 const contentToCache = [
-    "Build/ClashNet.loader.js",
-    "Build/ClashNet.framework.js",
-    "Build/ClashNet.data",
-    "Build/ClashNet.wasm",
-    "TemplateData/style.css"
-
+  "***.loader",
+  "***.framework.js.br",
+  "***.data.br",
+  "***.wasm.br",
+  "TemplateData/style.css",
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
+self.addEventListener("install", function (e) {
+  console.log("[Service Worker] Install");
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => {
+            return name !== cacheName;
+          })
+          .map((name) => {
+            console.log("[Service Worker] Deleting old cache:", name);
+            return caches.delete(name);
+          })
+      );
+    })
+  );
+
+  e.waitUntil(
+    (async function () {
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
+      console.log("[Service Worker] Caching all: app shell and content");
       await cache.addAll(contentToCache);
-    })());
+    })()
+  );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    (async function () {
       let response = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+      if (response) {
+        return response;
+      }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
-    })());
+    })()
+  );
 });
